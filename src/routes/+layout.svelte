d409a95baae6053bc4e3dd7b61e18b0989b7e4c3
INJAGA <47142905+INJAGA@users.noreply.github.com>
2025-01-24 13:27:00 +0900

    TailwindCSS v4, daisyUI v5, UnoCSS整備し、テーマ変更まで実装

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,79 @@
+<script>
+	import { themes } from '$lib/themes';
+	import { onMount } from 'svelte';
+	import 'virtual:uno.css';
+	import '../app.css';
+
+	let { children } = $props();
+	const title = 'Svelte5 & daisyUI テンプレート';
+	let currentTheme = $state(themes[0]);
+	let scrollY = $state(0);
+
+	onMount(() => {
+		currentTheme = localStorage.getItem('theme') ?? themes[0];
+	});
+
+	$effect(() => {
+		document.documentElement.setAttribute('data-theme', currentTheme);
+		localStorage.setItem('theme', currentTheme);
+	});
+</script>
+
+<svelte:window bind:scrollY />
+
+<div
+	class="bg-primary text-primary-content sticky top-0 flex items-center p-1 text-4xl transition duration-700"
+	class:shadow-2xl={scrollY !== 0}
+>
+	<div class="flex-1">{title}</div>
+	<div class="join flex flex-none items-center">
+		<button
+			title="ライトテーマへ"
+			aria-label="ライトテーマへ"
+			class="btn btn-ghost btn-square join-item"
+			onclick={() => (currentTheme = 'light')}
+		>
+			<span class="i-line-md:sunny-filled-loop"></span>
+		</button>
+		<button
+			title="ダークテーマへ"
+			aria-label="ダークテーマへ"
+			class="btn btn-ghost btn-square join-item"
+			onclick={() => (currentTheme = 'dark')}
+		>
+			<span class="i-line-md:moon-filled-loop"></span>
+		</button>
+		<div title="テーマを変更する" class="dropdown dropdown-end">
+			<div tabindex="0" role="button" class="btn btn-ghost join-item block">
+				<div class="flex h-full items-center gap-0.5">
+					<div class="i-ic:baseline-color-lens"></div>
+					<div class="i-material-symbols:keyboard-arrow-down"></div>
+				</div>
+			</div>
+			<ul
+				class="text-base-content bg-base-300 dropdown-content menu rounded-box z-1 h-[30rem] max-h-[calc(100vh-10rem)] flex-nowrap overflow-y-auto p-2 shadow-2xl"
+			>
+				{#each themes as theme}
+					<li>
+						<button
+							class="flex items-center"
+							class:menu-active={theme === currentTheme}
+							onclick={() => (currentTheme = theme)}
+						>
+							<div class="flex-1 capitalize">
+								{theme}
+							</div>
+							<div class="flex flex-none items-center gap-1 border p-1.5" data-theme={theme}>
+								<div class="badge badge-primary">A</div>
+								<div class="badge badge-secondary">A</div>
+								<div class="badge badge-neutral">A</div>
+								<div class="badge badge-accent">A</div>
+							</div>
+						</button>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</div>
+{@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [tailwindcss(), UnoCSS(), sveltekit()]
 });
